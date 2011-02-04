30fc2cb17a0071fd85d119d8942ddea118402f48
karthik <karthik@52ad589b-3343-cacd-a03e-9c4ee434cc52>
2011-02-04 21:08:54 +0000

    2012 Chennai

--- calc/update_all_cal.sh
+++ calc/update_all_cal.sh
@@ -4,3 +4,4 @@ chmod +x write_panchangam_tex_compre.py gen_cal.sh
 ./gen_cal.sh Zurich 47:22:48 8:32:24 'Europe/Zurich'
 ./gen_cal.sh London 51:30 0:07 'Europe/London'
 ./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' 2011 
+./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' 2012 
