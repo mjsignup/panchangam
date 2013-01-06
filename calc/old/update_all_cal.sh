86df879c699899db33872b8cd3991673d5c98070
karthik.raman <karthik.raman@gmail.com>
2013-01-06 16:44:25 +0000

    reorganisation

--- calc/old/update_all_cal.sh
+++ calc/old/update_all_cal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x write_panchangam_tex.py gen_cal.sh
+chmod +x write_panchangam_tex_compre.py gen_cal.sh
 ./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' #Place Lat Lon TZ
 ./gen_cal.sh Zurich 47:22:48 8:32:24 'Europe/Zurich'
 ./gen_cal.sh London 51:30 0:07 'Europe/London'
