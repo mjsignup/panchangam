38a99ef82e008edbc8863e8a1268d281c229b030
karthik.raman <karthik.raman@52ad589b-3343-cacd-a03e-9c4ee434cc52>
2013-12-29 09:29:15 +0000

    fix to accommodate icalendar API update

--- calc/update_all_cal.sh
+++ calc/update_all_cal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x write_panchangam_tex.py gen_cal.sh
 ./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' #Place Lat Lon TZ
-./gen_cal.sh Zurich 47:22:48 8:32:24 'Europe/Zurich'
-./gen_cal.sh London 51:30 0:07 'Europe/London'
-./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' 2013
-#./gen_cal.sh London 51:30 0:07 'Europe/London' 2013
+./gen_cal.sh Zurich 47:22:48 8:32:24 'Europe/Zurich' 2014
+./gen_cal.sh London 51:30:00 0:07:00 'Europe/London' 2014
+./gen_cal.sh Chennai 13:05:24 80:16:12 'Asia/Calcutta' 2014
+./gen_cal.sh Mumbai 18:57:36 72:49:12 'Asia/Calcutta' 2014
